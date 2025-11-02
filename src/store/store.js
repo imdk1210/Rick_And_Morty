ba2acf29ca165033130a25586a3071fefa6f46f4
Student <student@university.kg>
2025-11-02 23:31:30 +0600

    Lab 7: Add auth reducer and auth middleware for login

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,17 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import postsReducer from "./slices/postsSlice";
 import favoritesReducer from "./slices/favoriteSlice";
+import authReducer from "./slices/authSlice";
+import authMiddleware from "./middleware/authMiddleware";
 
 const rootReducer = combineReducers({
   post: postsReducer,
   favorite: favoritesReducer,
+  auth: authReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat([authMiddleware]),
 });
